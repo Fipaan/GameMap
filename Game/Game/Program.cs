16ec6fc2c68060a715808b3f585de51ab5976327
Fipaan <okostenyy@mail.ru>
2022-09-07 19:23:41 +0600

    Rework old functions 0.2 +sort functions by class

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -1,4 +1,8 @@
-﻿
+﻿using map;
+using static map.Enums;
+using static map.Map;
+using static map.Enums;
+
 namespace game
 {
     public class Program
@@ -6,13 +10,19 @@ namespace game
         public static void Main(string[] args)
         {
             Map game = new();
-            int[] size = { 15, 15 };
+            int[] size = { 15, 25 };
             TypesPattern pattern = TypesPattern.FullDots;
-            System.Object[,] arguments =
+            System.Object?[,] arguments =
             {
-                { Arguments.Blocks, Arguments.left, Arguments.one, Blocks.Hero, null, null, null, 1 },
-                { Arguments.Blocks, Arguments.right, Arguments.one, Blocks.Win, null, null, null, 1 },
-                { Arguments.Blocks, Arguments.hollow, Arguments.one, Blocks.Enemy, new int[]{3, 3}, new int[]{size[0] - 3, size[1] - 3}, null, 7 }
+
+                { Arguments.Blocks, Arguments.lineX, false, true, Blocks.Enemy, 2, 3},
+                { Arguments.Blocks, Arguments.lineX, false, true, Blocks.Enemy, 6, 3 },
+                { Arguments.Blocks, Arguments.lineX, false, true, Blocks.Enemy, 10, 3 },
+                { Arguments.Blocks, Arguments.lineX, false, true, Blocks.Enemy, 14, 3 },
+                { Arguments.Blocks, Arguments.lineX, false, true, Blocks.Enemy, 18, 3 },
+                { Arguments.Blocks, Arguments.lineX, false, true, Blocks.Enemy, 22, 3 },
+                { Arguments.Blocks, Arguments.left, false, true, Blocks.Hero, 1, null},
+                { Arguments.Blocks, Arguments.right, false, true, Blocks.Win, 1, null}
             };
             Map.Setup(size, pattern, arguments, ref game.field);
             Map.ShowField(game.field);
